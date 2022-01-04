12a4e82f6510194725f3be738f0eb9683018153b
Hoang Nghia Viet <viethoangnghia@gmail.com>
2022-01-04 22:32:40 +0700

    Change variable from run -> running

--- Setting_Window.py
+++ Setting_Window.py
@@ -4,14 +4,14 @@ pygame.init()
 Setting_Screen = pygame.display.set_mode((Setting_Window_Width , Setting_Window_Height))
 
 def Run():
-	Run = True
-	while Run:
+	running = True
+	while running:
 		Setting_Screen.fill((120 , 120 , 120))
 		# Setting_Screen.blit(Setting_Window_Background , (0 , 0))
 
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
-				Run = False
+				running = False
 
 		pygame.display.flip()
 
