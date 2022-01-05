6b90a071d876c0aed5b48197eda6dcb48e0d64fa
ttviet2805 <ttviet2805@gmail.com>
2022-01-05 11:24:24 +0700

    Add hover

--- button.py
+++ button.py
@@ -1,21 +1,25 @@
 import pygame
+from List_Modunle import *
+
 class Button() :
 	def __init__(self , x , y , image , scale):
 		width = image.get_width()
 		height = image.get_height() 
 		self.image = pygame.transform.scale(image , (int(width * scale) , int(height * scale)))
+		self.image.set_colorkey(Black)
+
 		self.rect = self.image.get_rect()
 		self.rect.topleft = (x,y)
 		self.clicked = False
 
 	def draw(self , screen):
-		#pos = pygame.mouse.get_pos()
-		#if self.rect.collidepoint(pos) :
-		#	if(pygame.mouse.get_pressed()[0] == 1  and self.clicked == False) :
-		#		self.clicked = True 
-		#		print("ok")
-		#if(pygame.mouse.get_pressed()[0] == 0) : 
-		#	self.clicked = False 
+		pos = pygame.mouse.get_pos()
+
+		if self.rect.collidepoint(pos):
+			self.image.set_alpha(255)
+			print(1)
+		else:
+			self.image.set_alpha(220)
 		screen.blit(self.image , (self.rect.x , self.rect.y))
 
 	def isClicked(self , screen): 
