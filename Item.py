6480afb784be47151c60968cb4a9eed206aa5b36
ttviet2805 <ttviet2805@gmail.com>
2022-01-08 16:07:29 +0700

    add Item

--- Item.py
+++ Item.py
@@ -2,13 +2,27 @@ from math import *
 from CONST import *
 
 class Item:
-	def __init__(self, X, Y, image):
-		self.X = X;
-		self.Y = Y;
-		self.image = Item_Image[0]
+	def Rand_
+	def __init__(self, X, Y, state):
+		self.state = state
+		self.frames = Item_Frame[state]
+		self.counter = 0
+		self.delay = 0.6 
+		self.index = 0
+		self.image = self.frames[0]
+		self.rect = self.image.get_rect()
+		self.rect.x = X 
+		self.rect.y = Y
 
 	def Draw(self, Screen) :
-		Row = self.X * 48 + 264
-		Col = self.Y * 48
+		self.counter += 1
 
-		Screen.blit(self.image, (Row, Col))
+		if self.counter > self.delay:
+			self.counter = 0
+			self.index += 1
+
+			if(self.index >= len(self.frames)):
+				self.index = 0
+
+		screen.blit(self.image, self.rect)
+			
