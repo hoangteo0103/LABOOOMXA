af26739fa0633f03fac70c4bc1a2ebc8787732cd
Hoang Nghia Viet <viethoangnghia@gmail.com>
2022-01-13 21:53:07 +0700

    Fix left right button

--- Map_Window.py
+++ Map_Window.py
@@ -12,16 +12,18 @@ def Run() :
 	Left_Button = Button(100, 335, Left_Image, 1)
 	Right_Button = Button(935, 335, Right_Image, 1)
 
+	Num_Map = 1
+	Cur_Map = 0
+
 	pygame.time.delay(300)
 	Map_Window_Width = 1080
 	Map_Window_Height = 720
 	Image_Width = 17 * 36
 	Image_Height = 15 * 36
-	Map_Image_List = [pygame.transform.scale(pygame.image.load(f'Game_Data/Image/Map_Image/Map{i}.png') , (Image_Width , Image_Height)) for i in range(1 , 2)]
+	Map_Image_List = [pygame.transform.scale(pygame.image.load(f'Game_Data/Image/Map_Image/Map{i}.png') , (Image_Width , Image_Height)) for i in range(1 , Num_Map + 1)]
 	Map_Button_List = []
 	for i in Map_Image_List:
 		Map_Button_List.append(Button(Map_Window_Width // 2 - Image_Width // 2 , Map_Window_Height // 2 - Image_Height // 2 , i , 1))
-		# Map_Screen.blit(i , (Map_Window_Width // 2 - Image_Width // 2 , Map_Window_Height // 2 - Image_Height // 2))
 	Map_Screen = pygame.display.set_mode((Map_Window_Width, Map_Window_Height))
 	running = True	
 	pygame.display.set_caption('LABOOMXA')
@@ -40,12 +42,18 @@ def Run() :
 		Left_Button.draw(Map_Screen)
 		Right_Button.draw(Map_Screen)
 
-		for i in Map_Button_List:
-			i.draw(Map_Screen)
+		Map_Button_List[Cur_Map].draw(Map_Screen)
 
-		for i in range(1 , 2):
-			if Map_Button_List[i - 1].isClicked(Map_Screen):
-				Game_Window.Run(i)
+		if Left_Button.isClicked(Map_Screen):
+			Cur_Map += (Num_Map - 1)
+			Cur_Map %= Num_Map
+		if Right_Button.isClicked(Map_Screen):
+			Cur_Map += 1
+			Cur_Map %= Num_Map
+
+		for i in range(0 , Num_Map):
+			if Map_Button_List[i].isClicked(Map_Screen):
+				Game_Window.Run(i + 1)
 				exit()
 		
 		pygame.display.update()
