1409d975bbb5849c59fc23c445c41411e8586a48
ttviet2805 <ttviet2805@gmail.com>
2022-01-17 22:15:19 +0700

    New update

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from List_Modunle import *
 import Main_Menu_Window
+from pygame import mixer
+
+mixer.init()
+
+mixer.music.load("Game_Data/Sound/Your_Smile.mp3")
+
+mixer.music.set_volume(0.5)
+
+mixer.music.play(-1)
+
 Main_Menu_Window.Run()
